e12f813c37ec88149d8f0fd33c3decc93ba2e9e5
Hossein <hosseinyaghoubi919@gmail.com>
2023-06-01 12:03:40 +0330

    set model admin

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['task', 'is_done', 'created_date', 'done_date']
--- todo/models.py
+++ todo/models.py
@@ -12,3 +12,6 @@ class Task(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     done_date = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.task
+
