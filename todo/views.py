bb387a7b979c45f31d335131519d1d5a31924830
Hossein <hosseinyaghoubi919@gmail.com>
2023-06-01 13:55:08 +0330

    create task view

--- todo/views.py
+++ todo/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView
 
 from todo.forms import CreatTaskForm
 from todo.models import Task
 
 
-class TaskListView(ListView):
+class TaskListView(LoginRequiredMixin, ListView):
     template_name = "todo/home.html"
     context_object_name = "tasks"
 
@@ -18,3 +19,20 @@ class TaskListView(ListView):
         context = super().get_context_data(**kwargs)
         context['form'] = CreatTaskForm()
         return context
+
+
+class CreateTaskView(LoginRequiredMixin, CreateView):
+    model = Task
+    fields = ('task', 'task_description')
+    template_name = "task/home.html"
+    success_url = reverse_lazy("task_list")
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(CreateTaskView, self).form_valid(form)
+
+        # new_task = form.save(commit=False)
+        # if self.request.user.is_authenticated:
+        #     new_task.user = self.request.user
+        #     new_task.save()
+        #     return super().form_valid(form)
