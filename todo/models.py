c1ba9485a0f208b8f2019ae0e6449f4565af2303
Hossein <hosseinyaghoubi919@gmail.com>
2023-06-01 09:52:53 +0330

    create model task

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,
+                             related_name="tasks")
+    task = models.CharField(max_length=500)
+    task_description = models.TextField()
+    is_done = models.BooleanField(default=False)
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    done_date = models.DateTimeField(auto_now=True)
+
